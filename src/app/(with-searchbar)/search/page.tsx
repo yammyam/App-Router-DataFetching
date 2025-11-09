afcb0eebf21cd5949922533450625e4769b9289b
yammyam <tkdxo999@gmail.com>
2025-11-09 20:17:55 +0900

    넥스트 스트리밍기능

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,11 +1,13 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
+import { delay } from "@/util/delay";
 
 export default async function Page({
   searchParams,
 }: {
   searchParams: Promise<{ q?: string }>; //15버전부터 searchParams와 Params는 프로미스객체이므로 Promise<>타입 명시해줘야함
 }) {
+  await delay(1500);
   const { q } = await searchParams;
   //현재페이지의 쿼리스트링을 가져오는 ,페이지 컴포넌트의 자동으로 제공되는 기능
   const response = await fetch(
