3fce02baa395ae81ae2090aa0d7de8a4894a1e63
yammyam <tkdxo999@gmail.com>
2025-11-11 18:37:31 +0900

    서버액션 함수 테스트

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -10,15 +10,10 @@ export function generateStaticParams() {
   // *주의할 점 : id 우측이 문자열일 것. 아래 직접패칭하는 코드가 없더라도 1,2,3번은 강제로 패칭되다는 점
 }
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string | string[] }>;
-}) {
-  const { id } = await params;
+async function BookDetail({ bookId }: { bookId: string | string[] }) {
   //아래는 직접 데이터를 패칭해오는 모습
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`
     //페이지컴포넌트의 자동으로 프롭스로 전달되는 url 파라미터의 값이다. 이름은 id
   );
 
@@ -33,7 +28,7 @@ export default async function Page({
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
 
   return (
-    <div className={style.container}>
+    <section>
       <div
         className={style.cover_img_container}
         style={{ backgroundImage: `url('${coverImgUrl}')` }}
@@ -46,6 +41,72 @@ export default async function Page({
         {author} | {publisher}
       </div>
       <div className={style.description}>{description}</div>
+    </section>
+  );
+}
+
+function ReviewEditor() {
+  async function createReviewAction(formData: FormData) {
+    "use server"; //서버액션 명시
+    const content = formData.get("content")?.toString();
+    const author = formData.get("author")?.toString();
+    console.log(content, author);
+  }
+  return (
+    <section>
+      <form action={createReviewAction}>
+        <input name="content" placeholder="리뷰를 적어주세요!" />
+        <input name="author" placeholder="작성자" />
+        <button type="submit">작성하기</button>
+      </form>
+    </section>
+  );
+}
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string | string[] }>;
+}) {
+  const { id } = await params;
+  return (
+    <div className={style.container}>
+      <BookDetail bookId={id} />
+      <ReviewEditor />
     </div>
   );
 }
+
+// const { id } = await params;
+// //아래는 직접 데이터를 패칭해오는 모습
+// const response = await fetch(
+//   `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`
+//   //페이지컴포넌트의 자동으로 프롭스로 전달되는 url 파라미터의 값이다. 이름은 id
+// );
+
+// if (!response.ok) {
+//   if (response.status === 404) {
+//     notFound();
+//   }
+//   return <div>오류가 발생했습니다....</div>;
+// }
+// const book = await response.json();
+
+// const { title, subTitle, description, author, publisher, coverImgUrl } = book;
+
+// return (
+//   <div className={style.container}>
+//     <div
+//       className={style.cover_img_container}
+//       style={{ backgroundImage: `url('${coverImgUrl}')` }}
+//     >
+//       <img src={coverImgUrl} />
+//     </div>
+//     <div className={style.title}>{title}</div>
+//     <div className={style.subTitle}>{subTitle}</div>
+//     <div className={style.author}>
+//       {author} | {publisher}
+//     </div>
+//     <div className={style.description}>{description}</div>
+//   </div>
+// );
