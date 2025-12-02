ebbcab1aea6be468ed816d74dc3e2ba4efa7b0cc
yammyam <tkdxo999@gmail.com>
2025-12-02 16:33:26 +0900

    뒷배경 패럴랠라우트 적용

--- src/app/@modal/(.)book/[id]/page.tsx
+++ src/app/@modal/(.)book/[id]/page.tsx
@@ -2,10 +2,8 @@ import Modal from "@/components/modal";
 import BookPage from "@/app/book/[id]/page";
 export default function Page(props: any) {
   return (
-    <div>
-      <Modal>
-        <BookPage {...props} />
-      </Modal>
-    </div>
+    <Modal>
+      <BookPage {...props} />
+    </Modal>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
 import { BookData } from "@/types";
+import { ReactNode } from "react";
 
 async function Footer() {
   const response = await fetch(
@@ -24,8 +25,10 @@ async function Footer() {
 
 export default function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -37,6 +40,7 @@ export default function RootLayout({
           <main>{children}</main>
           <Footer />
         </div>
+        {modal}
         <div id="modal-root"></div>
       </body>
     </html>
